60e7f2a975d2a77d97e2fcc06da2b85692363c80
sacarvy <sukhpreetben10@gmail.com>
2021-08-17 16:56:41 +0530

    V1 Made Contact page

---
+++ src/components/Underline.svelte
@@ -0,0 +1,22 @@
+<script>
+import { onMount } from 'svelte';
+import { annotate } from 'rough-notation';
+export let color;
+let e;
+onMount(()=>{
+  // a for annotation
+  const a = annotate( e, {
+    type:'underline',
+    color: color,
+    rtl:false,
+    animate:true,
+    padding:10,
+    animationDuration:500,
+    iterations:2,
+  });
+  a.show();
+});
+</script>
+<span bind:this={e}>
+  <slot/>
+</span>
